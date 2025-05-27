5cdd0d2e41e5aa9ae46f2b2bbf77b212d8eb3c0a
Kauam Scalzer <kauam.souza@matrixgo.ai>
2025-05-27 17:16:35 -0300

    feat: add logo

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { addIcons } from 'ionicons';
+import {
+  logoGoogle,
+} from 'ionicons/icons';
+
+addIcons({
+  'logo-google': logoGoogle,
+});
 
 @Component({
   selector: 'app-login',
